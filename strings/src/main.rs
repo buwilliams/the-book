d5df0238f2a81d242c134ffc4dc3a9c5dbad8165
Buddy Williams <buddywilliams@gmail.com>
2021-03-16 15:15:25 -0400

    Using format! for string concatenation

--- strings/src/main.rs
+++ strings/src/main.rs
@@ -1,8 +1,8 @@
 fn main() {
     let mut s: String = String::new();
     s.push_str("Hello");
-    println!("{}", s);
-    s.push(' ');
-    s.push_str("World!");
-    println!("{}", s);
+    let mut w = String::new();
+    w.push_str("World!");
+    let hello = format!("{} {}", s, w);
+    println!("{}", hello);
 }
